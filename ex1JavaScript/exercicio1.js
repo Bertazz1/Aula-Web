147a51bb5bd46eb3a7392681c1ecd1505a6e5edf
Gustavo <gustavobertuzziif@gmail.com>
2024-10-07 22:22:51 -0300

    fdgdfgdf

--- ex1JavaScript/exercicio1.js
+++ ex1JavaScript/exercicio1.js
@@ -5,27 +5,28 @@ const resp1 = document.querySelector("h2");
 const resp2 = document.querySelector("h3")
 
 
-frm.addEventListener("submit",(e) =>{
+frm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    
-    const nome=frm.value;
-    const nota1=Number(frm.inNota1.value);
-    const nota2=Number(frm.inNota2.value);
-    const nota3=Number(frm.inNota3.value);
-    const nota4=Number(frm.inNota4.value);
-    const media=(nota1+nota2+nota3+nota4)/4;
+
+    const nome = frm.value;
+    const nota1 = Number(frm.inNota1.value);
+    const nota2 = Number(frm.inNota2.value);
+    const nota3 = Number(frm.inNota3.value);
+    const nota4 = Number(frm.inNota4.value);
+    const media = (nota1 + nota2 + nota3 + nota4) / 4;
 
     console.log(nome);
     resp1.innerText = `Olá ${nome} seja bem vindo, sua média é: ${media}`;
-    if(media>=7.0){
-        resp2.innerText =`Você foi aprovado`
+    if (media >= 7.0) {
+        resp2.innerText = `Você foi aprovado`
         resp2.style.color = "green"
-    }else if(media<=6.9&&media>=4){
-    resp2.innerText =`Você está de recuperação`
-    resp2.style.color = "blue"
-    }else{ resp2.innerText =`Você mamou`
-    resp2.style.color = "red"}
+    } else if (media >= 4) {
+        resp2.innerText = `Você está de recuperação`
+        resp2.style.color = "blue"
+    } else {
+        resp2.innerText = `Você mamou`
+        resp2.style.color = "red"
+    }
 })
 
-    
